7d75fc188f60e31d491692c09f9d9519c7dfaae9
Mariano Ordano <mariano.ordano@gmail.com>
2026-05-17 17:04:56 -0300

    check
    testing

--- scripthugo.R
+++ scripthugo.R
@@ -14,16 +14,24 @@ blogdown::new_site(theme = "theNewDynamic/gohugo-theme-ananke")
 # local host
 
 blogdown::serve_site()
+# checked with Claude assistance
 
+# with each change, run: 
+blogdown::stop_server()
+blogdown::serve_site()
+
+# at the end of local changes
+# check the local web
+# satisfied before to github desktop, run:
 blogdown::build_site()
 
-# checked with Claude assistance
 # github desktop
 # commit
 # push origin
 
 # check web
 
+##### apuntes
 # incluir versionado
 # incluir texto explicativo del trabajo
 # incluir bitacora con una forma de la experticia
